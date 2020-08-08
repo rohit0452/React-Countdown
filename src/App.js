ef28627f5da068874c541e294e2fd5d79cabd695
rohit <57873071+rohit0452@users.noreply.github.com>
2020-08-08 13:46:50 +0530

    +Rohit | Some Changes Needed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,97 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component,useState, useRef, useEffect } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Countdown() {
+    const [timerDays, setTimerDays] = useState("00");
+    const [timerHours, setTimerHours] = useState("00");
+    const [timerMinutes, setTimerMinutes] = useState("00");
+    const [timerSeconds, setTimerSeconds] = useState("00");
+    
+
+    
+  
+
+    let interval = useRef()
+
+    // timer start
+
+    const startTimer = () => {
+        const countdownDate = new Date("September 30, 2020 00:00:00").getTime();
+       
+       // const countdownDate2 = countdownDate + Date(+30)
+     
+        
+        
+
+        interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = countdownDate - now;
+            const days = Math.floor((distance / (1000 * 60 * 60 * 24)))
+            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+            const seconds = Math.floor((distance % (1000 * 60)) / 1000)
+
+            if (distance < 0) {
+                // stop
+                clearInterval(interval.current)
+            } else {
+                setTimerDays(days);
+                setTimerHours(hours);
+                setTimerMinutes(minutes);
+                setTimerSeconds(seconds);
+            
+            }
+
+        }, 1000)
+    }
+
+    // Components DId Mount
+
+    useEffect(() => {
+        startTimer();
+        return () => {
+            clearInterval(interval.current)
+        }
+    })
+
+    return (
+        <section className="timer-container">
+            <section className="timer">
+                <div className="head">
+                    <h1>Countdown Timer</h1>
+                    <h3>Your Countdown will be Expired In</h3>
+                </div>
+                <div className="maintimer">
+                    <div className="abc">
+                        <p>{timerDays} <span>      :</span></p>
+
+                        <p><small>Days</small></p>
+
+                    </div>
+
+                    <div className="abc">
+                        <p>{timerHours} <span>     :</span></p>
+                        <p><small>Hours</small></p>
+                    </div>
+
+                    <div className="abc">
+                        <p>{timerMinutes} <span>   :</span></p>
+                        <p><small>Minutes</small></p>
+                    </div>
+
+                    <div className="abc">
+                        <p>{timerSeconds}</p>
+                        <p><small>Seconds</small></p>
+                    </div>
+
+
+                </div>
+         
+            </section>
+
+        </section>
+    )
 }
 
-export default App;
+export default Countdown;
